3e770fcbad1d3f78e330d16c7932d6e5a900f551
Asror <a.abdurakhmonoff@gmail.com>
2023-03-06 22:34:42 +0500

    Setup Mapbox Maps SDK

--- app/src/main/java/com/abdurakhmanov/ridez/ui/MainActivity.kt
+++ app/src/main/java/com/abdurakhmanov/ridez/ui/MainActivity.kt
@@ -2,11 +2,19 @@ package com.abdurakhmanov.ridez.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.abdurakhmanov.ridez.R
+import com.abdurakhmanov.ridez.databinding.ActivityMainBinding
+import com.mapbox.maps.Style
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
     }
 }
