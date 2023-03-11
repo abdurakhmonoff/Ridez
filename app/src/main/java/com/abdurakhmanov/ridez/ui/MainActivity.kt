c3dab1ef7134c3f0bd61faeb7f89268b06a54140
Asror <a.abdurakhmonoff@gmail.com>
2023-03-11 23:20:41 +0500

    Implement the main screen UI

--- app/src/main/java/com/abdurakhmanov/ridez/ui/MainActivity.kt
+++ app/src/main/java/com/abdurakhmanov/ridez/ui/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.abdurakhmanov.ridez.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.abdurakhmanov.ridez.databinding.ActivityMainBinding
+import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.Style
-
-private lateinit var binding: ActivityMainBinding
+import com.mapbox.maps.plugin.animation.camera
+import com.mapbox.maps.plugin.attribution.attribution
+import com.mapbox.maps.plugin.compass.compass
+import com.mapbox.maps.plugin.logo.logo
+import com.mapbox.maps.plugin.scalebar.scalebar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +21,25 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
+        binding.mapView.apply {
+            getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
+            attribution.enabled = false
+            logo.enabled = false
+            compass.enabled = false
+            scalebar.enabled = false
+        }
+
+        binding.btnZoomIn.setOnClickListener {
+            binding.mapView.camera.flyTo(
+                CameraOptions.Builder().zoom(binding.mapView.getMapboxMap().cameraState.zoom + 1.0)
+                    .build()
+            )
+        }
+        binding.btnZoomOut.setOnClickListener {
+            binding.mapView.camera.flyTo(
+                CameraOptions.Builder().zoom(binding.mapView.getMapboxMap().cameraState.zoom - 1.0)
+                    .build()
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/abdurakhmanov/ridez/ui/widgets/StatusButtonToggleGroup.kt
@@ -0,0 +1,62 @@
+package com.abdurakhmanov.ridez.ui.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.annotation.ColorInt
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import com.abdurakhmanov.ridez.R
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.button.MaterialButtonToggleGroup
+
+class StatusButtonToggleGroup @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : MaterialButtonToggleGroup(context, attrs, defStyleAttr) {
+
+    private val checkedFreeColor = ContextCompat.getColor(context, R.color.aqua_green)
+    private val checkedFreeTextColor = ContextCompat.getColor(context, R.color.lynx_white)
+    private val checkedBusyColor = ContextCompat.getColor(context, R.color.valencia_red)
+    private val checkedBusyTextColor = ContextCompat.getColor(context, R.color.lynx_white)
+
+    private val uncheckedColor = ContextCompat.getColor(context, R.color.lynx_white)
+    private val uncheckedTextColor = ContextCompat.getColor(context, R.color.midnight_black)
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        updateButtonColors()
+        addOnButtonCheckedListener { buttonGroup, checkedId, isChecked ->
+            if (isChecked) {
+                if (checkedId == buttonGroup[0].id) {
+                    val checkedFreeButton = findViewById<MaterialButton>(checkedId)
+                    setButtonColor(checkedFreeButton, checkedFreeColor, checkedFreeTextColor)
+                } else {
+                    val checkedBusyButton = findViewById<MaterialButton>(checkedId)
+                    setButtonColor(checkedBusyButton, checkedBusyColor, checkedBusyTextColor)
+                }
+            } else {
+                val uncheckedButton = findViewById<MaterialButton>(checkedId)
+                setButtonColor(uncheckedButton, uncheckedColor, uncheckedTextColor)
+            }
+        }
+    }
+
+    private fun updateButtonColors() {
+        for (i in 0 until childCount) {
+            val button = getChildAt(i) as MaterialButton
+            if (button.isChecked) {
+                setButtonColor(button, checkedFreeColor, checkedFreeTextColor)
+            } else {
+                setButtonColor(button, uncheckedColor, uncheckedTextColor)
+            }
+        }
+    }
+
+    private fun setButtonColor(
+        button: MaterialButton, @ColorInt backgroundColor: Int, @ColorInt textColor: Int
+    ) {
+        button.setBackgroundColor(backgroundColor)
+        button.setTextColor(textColor)
+    }
+}
